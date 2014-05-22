62da69389c69746f4291a3cfd8b375e28609a6ef
dianlujitao <dianlujitao@gmail.com>
2014-05-22 21:51:33 +0800

    enable build mm-video & libI420colorconvert for msm7x27a
    Change-Id: I10cba2cce53bdc22e6a5080b1578c20ce623dada

--- Android.mk
+++ Android.mk
@@ -5,7 +5,7 @@ include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
 include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
 endif
 
-ifneq ($(filter msm8974 msm8960 msm8660 msm7x30,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8974 msm8960 msm8660 msm7x30 msm7x27a,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/mm-video/Android.mk
 include $(QCOM_MEDIA_ROOT)/libI420colorconvert/Android.mk
 endif
